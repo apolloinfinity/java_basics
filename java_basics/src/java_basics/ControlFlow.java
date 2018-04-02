3552a9f244b0e33adcd54350687e4e59c7e62bd5
Javier <apollo.infinity88@gmail.com>
2018-04-02 16:19:58 -0700

    Finished lesson on the basics of array with Java

---
+++ java_basics/src/java_basics/ControlFlow.java
@@ -0,0 +1,17 @@
+package java_basics;
+
+public class ControlFlow {
+
+	public static void main(String[] args) {
+		
+		boolean hungry = false; 
+		
+		if(hungry == true) {
+			System.out.println("I'm starving...");
+		} else {
+			System.out.println("I'm not hungry");
+		}
+
+	}
+
+}
