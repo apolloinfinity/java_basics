20b98d60f34bff2f54cc0291aefa9ab96995e5b7
Javier Ramirez <apollo.infinity88@gmail.com>
2019-12-11 08:04:04 -0800

    Updating project files

---
+++ Java8Project/src/Java8Project/Test.java
@@ -0,0 +1,10 @@
+package Java8Project;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+	}
+
+}
---
+++ java_basics/src/java_basics/LearningMethods.java
@@ -0,0 +1,15 @@
+package java_basics;
+
+public class LearningMethods {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Dr. Ocelot");
+		printSomeJunk();
+
+	}
+
+	public static void printSomeJunk() {
+		System.out.println("Some bla bla blah");
+	}
+}
